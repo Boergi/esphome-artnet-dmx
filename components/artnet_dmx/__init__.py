76c486949a44d947447730b6df5a90a3967dd5fe
Boergi <boergim@gmail.com>
2025-08-10 11:07:49 +0200

    Korrigiere die Verwendung von GenerateID in der CONFIG_SCHEMA für ArtNetDMXOutput

--- components/artnet_dmx/__init__.py
+++ components/artnet_dmx/__init__.py
@@ -1,8 +1,8 @@
-# registers the "artnet_dmx:" YAML block for ESPHome 2025.x
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome import pins
+from esphome.const import CONF_ID  # <— HIER importieren
 
 CONF_DERE_PIN = "dere_pin"
 CONF_UNIVERSE = "universe"
@@ -12,7 +12,7 @@ artnet_ns = cg.esphome_ns.namespace("artnet_dmx")
 ArtNetDMXOutput = artnet_ns.class_("ArtNetDMXOutput", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ArtNetDMXOutput),
+    cv.GenerateID(CONF_ID): cv.declare_id(ArtNetDMXOutput),
     cv.Required(CONF_DERE_PIN): pins.gpio_output_pin_schema,
     cv.Required(CONF_UNIVERSE): cv.int_range(min=0, max=32767),
     cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
@@ -21,7 +21,7 @@ CONFIG_SCHEMA = cv.Schema({
 DEPENDENCIES = ["uart"]
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cg.CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])  # <-- jetzt korrekt
     dere = await cg.gpio_pin_expression(config[CONF_DERE_PIN])
     cg.add(var.set_dere_pin(dere))
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
